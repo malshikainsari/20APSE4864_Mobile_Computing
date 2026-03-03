d830d8e2b263e5a8cff3ecc27a7d93fa6b6e1f22
malshikainsari <malshikainsari89@gmail.com>
2026-03-03 18:28:35 +0530

    Add SumOfEvenNumbers.kt

---
+++ KotlinPractice/SumOfEvenNumbers.kt
@@ -0,0 +1,16 @@
+fun main() {
+    sumOfEvenNumbers()  
+}
+
+fun sumOfEvenNumbers() {
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8)
+    var sum = 0
+
+    for (num in numbers) {
+        if (num % 2 == 0) {
+            sum += num
+        }
+    }
+
+    println("Sum: $sum")
+}
